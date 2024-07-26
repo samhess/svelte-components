ce8e46603e6a93f3cd761e9906634d9ee03c7abf
Samuel Hess <sam.hess@me.com>
2024-07-26 21:47:09 +0300

    fix path

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,5 @@
 import { readdir } from 'node:fs/promises'
+import { resolve } from 'node:path'
 import { error } from '@sveltejs/kit'
 
 /** @type {import('./$types').LayoutServerLoad} */
@@ -6,10 +7,9 @@ export async function load({locals, route}) {
   const {session, user} = locals
   const mainMenu = [ 'Home', 'Components', 'Examples' ]
   const {id:path} = route
-  console.log(path)
   if (path) {
     const [level1] = path.replace(/^\//,'').split('/')
-    const entries = await readdir(`./src/routes/${level1}`, {withFileTypes:true})
+    const entries = await readdir(resolve('./src/routes', level1), {withFileTypes:true})
     const subMenu = entries
       .filter(item => item.isDirectory())
       .map(({name, path}) => ({
