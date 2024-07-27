cc0f0d8e943e72055838d87a474ab08580c404b4
Samuel Hess <sam.hess@me.com>
2024-07-27 12:21:58 +0300

    fix routes

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,21 +1,30 @@
-import { readdir } from 'node:fs/promises'
-import { resolve } from 'node:path'
 import { error } from '@sveltejs/kit'
+const menuItems = {
+  components: Object.keys(import.meta.glob('./components/*/*.svelte')),
+  examples: Object.keys(import.meta.glob('./examples/*/*.svelte'))
+}
+function getSubMenu(mainMenuItem) {
+  if (mainMenuItem in menuItems) {
+    const subMenu = menuItems[mainMenuItem]
+    return subMenu.map(path => ({
+      name: path.match(/\/.*\/(.*)\//)[1].replace(/./,char=>char.toUpperCase()),
+      path: path.replace(/^\./,'').replace(/\/\+page.svelte$/,'')
+    }))
+  } else {
+    return []
+  }
+
+}
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({locals, route}) {
+
   const {session, user} = locals
   const mainMenu = [ 'Home', 'Components', 'Examples' ]
   const {id:path} = route
   if (path) {
     const [level1] = path.replace(/^\//,'').split('/')
-    const entries = await readdir(resolve(process.cwd(), 'database', level1), {withFileTypes:true})
-    const subMenu = entries
-      .filter(item => item.isDirectory())
-      .map(({name, path}) => ({
-        name: name.replace(/./,char=>char.toUpperCase()).replace(/-/g,' '),
-        path: `/${level1}/${name}`
-      }))
+    const subMenu = getSubMenu(level1)
     return {session, user, mainMenu, subMenu}
   } else {
     error(500, 'route does not exist')
